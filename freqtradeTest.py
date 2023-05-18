da69615adf0f9cf9fab46d74157ca8b1d8231f68
apple1003 <a123@123deMacBook-Pro.local>
2023-05-18 16:17:40 +0800

    fix test

--- freqtradeTest.py
+++ freqtradeTest.py
@@ -3,12 +3,12 @@ from freqtrade import main
 from freqtrade.strategy.strategy_helper import stoploss_from_open
 # freqtrade backtesting --timerange 20230506- -c config_fast_btc.json --strategy FastTradeStrategy
 
-CONFIG_PATH = '/Users/a123/work/quant/freqtrade/config_fast_btc.json'
+CONFIG_PATH_MAC = '/Users/a123/work/quant/freqtrade/config_fast_btc.json'
 CONFIG_PATH = 'D:\devlop\quant\\freqtrade\config_fast.json'
 
-backtesting = ["backtesting", "--timerange", "20230505-20230507", "-c", CONFIG_PATH, "--strategy", "FastTradeStrategy"]
-tryRun = ["trade", "-c", CONFIG_PATH,  "--strategy", "FastTradeStrategy"]
-download = ["download-data", "-c", CONFIG_PATH, "--timerange", "20230101-", "-t", "5m"]
+backtesting = ["backtesting", "--timerange", "20230505-20230507", "-c", CONFIG_PATH_MAC, "--strategy", "FastTradeStrategy"]
+tryRun = ["trade", "-c", CONFIG_PATH_MAC,  "--strategy", "FastTradeStrategy"]
+download = ["download-data", "-c", CONFIG_PATH_MAC, "--timerange", "20230101-", "-t", "5m"]
 
 
 def testStoploss(current_profit, is_short=False, leverage=25):
